8cc4ec487e28440e275ebc3f81529cd33a3a2564
Hemanth <raohemanth200@gmail.com>
2026-04-30 20:48:12 +0530

    Footer updates

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,6 +2,9 @@ import React from 'react'
 import assets from '../assets/assets'
 
 const Footer = ({theme}) => {
+
+  const year = new Date().getFullYear();
+  
   return (
     <div className='bg-slate-50 dark:bg-gray-900 pt-10 sm:pt-10 
     mt-20 sm:mt-40 px-4 sm:px-10 lg:px-24 xl:px-40'>
@@ -42,7 +45,7 @@ const Footer = ({theme}) => {
         {/* {footer bottom} */}
       <div className='pb-6 text-sm text-gray-500 flex 
       justify-center sm:justify-between gap-4 flex-wrap'>
-        <p>Copyright 2026 © Hemanth - All rights reserved.</p>
+        <p>Copyright {year} © Hemanth - All rights reserved.</p>
         <div className='flex items-center justify-between gap-4'>
             <img src={assets.facebook_icon} alt="" />
             <img src={assets.twitter_icon} alt="" />
